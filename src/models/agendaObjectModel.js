dd07355a9a93cd01043f95c77030c787b4538777
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-18 21:13:32 +0530

    created a schema for the agenda

--- src/models/agendaObjectModel.js
+++ src/models/agendaObjectModel.js
@@ -1,6 +1,16 @@
 //Importing the required packages
 const mongoose = require('mongoose');
 
+const agendaSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    status: { type: Boolean, required: true },
+    date: { type: Date, required: true },
+},
 
+    {
+        versionKey: false,
+        timestamps: true
+    });
 
 // title, description, statue, date, created at, updated at, etc
