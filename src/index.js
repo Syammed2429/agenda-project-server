59bf10387994c5296d9c83ee9fb4db9208e58da3
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-18 20:46:38 +0530

    assigning the port and using the express and cors

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+//assigning the port
+const port = process.env.PORT || 2924;
+
+//Assigning the express to the app variable
+const app = express();
+
+app.use(express.json());
+//Using cors to prevent from the cors errors
+app.use(cors());
+
