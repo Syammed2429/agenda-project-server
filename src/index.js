bce337474e495b90df2069707aba4ef12ef7d697
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-18 22:21:41 +0530

    imported agenda controller in index file and added a get point to it

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const agendaController = require('./controller/agendaController')
+
 //assigning the port
 const port = process.env.PORT || 2924;
 
@@ -13,6 +15,9 @@ app.use(express.json());
 
 //Using cors to prevent from the cors errors
 app.use(cors());
+
+//API Points
+app.use('/agenda', agendaController)
 
 //export the app and port 
 module.exports = {
