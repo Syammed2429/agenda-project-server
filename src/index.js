e33b66bee357e048c6da702b1eb990ee1683e6f0
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-20 00:21:29 +0530

    interchanged the positions of cors

--- src/index.js
+++ src/index.js
@@ -11,10 +11,11 @@ const port = process.env.PORT || 2924;
 //Assigning the express to the app variable
 const app = express();
 
-app.use(express.json());
-
 //Using cors to prevent from the cors errors
 app.use(cors());
+
+app.use(express.json());
+
 
 //API Points
 app.use('/agenda', agendaController)
